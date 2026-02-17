bad7bdc0308b1435b699d8618949529aa91bb68d
Adam Hind <adamhind005@gmail.com>
2026-02-17 12:33:38 -0800

    Update run_eval_minimal.py

--- run_eval_minimal.py
+++ run_eval_minimal.py
@@ -29,7 +29,6 @@ torch.manual_seed(SEED)
 if torch.cuda.is_available():
     torch.cuda.manual_seed_all(SEED)
 
-torch.use_deterministic_algorithms(True)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
